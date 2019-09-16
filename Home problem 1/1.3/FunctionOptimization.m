591e475033786e8275dd3728334b3096c4a8f63f
Rebecca Svensson <rebsven@student.chalmers.se>
2019-09-16 16:47:58 +0200

    Påbörjat 1.3.2

--- Home problem 1/1.3/FunctionOptimization.m
+++ Home problem 1/1.3/FunctionOptimization.m
@@ -18,38 +18,38 @@ for j = 1:nRuns
     fitness = zeros(populationSize,1);
     
     % Init plots
-    fitnessFigureHandle = figure;
-    hold on;
-    set(fitnessFigureHandle, 'Position', [50,50,500,200]);
-    set(fitnessFigureHandle, 'DoubleBuffer', 'on');
-    axis([1 numberOfGenerations 2.5 3]);
-    bestPlotHandle = plot(1:numberOfGenerations, zeros(1, numberOfGenerations));
-    textHandle = text(30, 2.6, sprintf('best: %4.3f', 0.0));
-    hold off;
-    drawnow;
-
-    surfaceFigureHandle = figure;
-    hold on;
-    set(surfaceFigureHandle, 'DoubleBuffer', 'on');
-    delta = 0.1;
-    limit = fix(2*variableRange/delta) + 1;
-    [xValues, yValues] = meshgrid(-variableRange:delta:variableRange, -variableRange:delta:variableRange);
-    zValues = zeros(limit, limit);
-
-    for j = 1:limit
-        for k = 1:limit
-            zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
-        end
-    end
-
-    surfl(xValues, yValues, zValues)
-    colormap gray;
-    shading interp;
-    view([-7 -9 10]);
-    decodedPopulation = zeros(populationSize, 2);
-    populationPlotHandle = plot3(decodedPopulation(:,1), decodedPopulation(:,2), fitness(:), 'kp');
-    hold off; 
-    drawnow;
+%     fitnessFigureHandle = figure;
+%     hold on;
+%     set(fitnessFigureHandle, 'Position', [50,50,500,200]);
+%     set(fitnessFigureHandle, 'DoubleBuffer', 'on');
+%     axis([1 numberOfGenerations 2.5 3]);
+%     bestPlotHandle = plot(1:numberOfGenerations, zeros(1, numberOfGenerations));
+%     textHandle = text(30, 2.6, sprintf('best: %4.3f', 0.0));
+%     hold off;
+%     drawnow;
+% 
+%     surfaceFigureHandle = figure;
+%     hold on;
+%     set(surfaceFigureHandle, 'DoubleBuffer', 'on');
+%     delta = 0.1;
+%     limit = fix(2*variableRange/delta) + 1;
+%     [xValues, yValues] = meshgrid(-variableRange:delta:variableRange, -variableRange:delta:variableRange);
+%     zValues = zeros(limit, limit);
+% 
+%     for j = 1:limit
+%         for k = 1:limit
+%             zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
+%         end
+%     end
+% 
+%     surfl(xValues, yValues, zValues)
+%     colormap gray;
+%     shading interp;
+%     view([-7 -9 10]);
+     decodedPopulation = zeros(populationSize, 2);
+%     populationPlotHandle = plot3(decodedPopulation(:,1), decodedPopulation(:,2), fitness(:), 'kp');
+%     hold off; 
+%     drawnow;
 
     % Initialize population
     population = InitializePopulation(populationSize, numberOfGenes);
@@ -106,12 +106,12 @@ for j = 1:nRuns
         population = tempPopulation;
 
         % Plot new generation
-        plotvector = get(bestPlotHandle, 'YData');
-        plotvector(iGeneration) = maximumFitness;
-        set(bestPlotHandle, 'YData', plotvector);
-        set(textHandle, 'String', sprintf('best*: %4.3f', maximumFitness));
-        set(populationPlotHandle, 'XData', decodedPopulation(:,1), 'YData', decodedPopulation(:,2), 'ZData', fitness(:));
-        drawnow;
+%         plotvector = get(bestPlotHandle, 'YData');
+%         plotvector(iGeneration) = maximumFitness;
+%         set(bestPlotHandle, 'YData', plotvector);
+%         set(textHandle, 'String', sprintf('best*: %4.3f', maximumFitness));
+%         set(populationPlotHandle, 'XData', decodedPopulation(:,1), 'YData', decodedPopulation(:,2), 'ZData', fitness(:));
+%         drawnow;
     end
 
     bestFitness(j) = maximumFitness;
@@ -121,4 +121,7 @@ for j = 1:nRuns
     disp('xBest');
     disp(xBest);
 end
+
+disp('median fitness');
+disp(median(bestFitness));
 
