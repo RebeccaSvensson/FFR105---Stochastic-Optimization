f75127b83d2ee51565abfdcb22d6f85194f4b9cd
Rebecca Svensson <rebsven@student.chalmers.se>
2019-09-23 10:51:23 +0200

    Program instructions added to report. 3a updated to correct version

--- Home problem 1/1.3/a/FunctionOptimization.m
+++ Home problem 1/1.3/a/FunctionOptimization.m
@@ -11,11 +11,17 @@ variableRange = 10.0;
 numberOfGenerations = 500;
 tournamentSize = 4;
 nCopies = 2;
+nRuns = 10;
+bestFitness = zeros(nRuns, 1);
 
-fitness = zeros(populationSize,1);
+globalMaximumFitness = 0;
+globalXBest = zeros(1,2); 
 
-decodedPopulation = zeros(populationSize, 2);
-% Init plots
+for j = 1:nRuns
+    fitness = zeros(populationSize,1);
+
+    decodedPopulation = zeros(populationSize, 2);
+    % Init plots
 %     fitnessFigureHandle = figure;
 %     hold on;
 %     set(fitnessFigureHandle, 'Position', [50,50,500,200]);
@@ -49,68 +55,84 @@ decodedPopulation = zeros(populationSize, 2);
 %     drawnow;
 
 
-% Initialize population
-population = InitializePopulation(populationSize, numberOfGenes);
+    % Initialize population
+    population = InitializePopulation(populationSize, numberOfGenes);
 
-for iGeneration = 1:numberOfGenerations
+    for iGeneration = 1:numberOfGenerations
 
-    maximumFitness = 0.0; % Assumes non-negative fitness values!
-    xBest = zeros(1,2); 
-    bestIndividualIndex = 0;
+        maximumFitness = 0.0; % Assumes non-negative fitness values!
+        xBest = zeros(1,2); 
+        bestIndividualIndex = 0;
 
-    % Calculate fitness score for entire population
-    for i = 1:populationSize
-        chromosome = population(i,:);
-        x = DecodeChromosome(chromosome, variablesPerChromosome, variableRange);
-        decodedPopulation(i,:) = x;
-        fitness(i) = EvaluateIndividual(x);
-        % Save info about best individual
-            if (fitness(i) > maximumFitness)
-                maximumFitness = fitness(i);
-                bestIndividualIndex = i;
-                xBest = x;
-            end
-    end
-
-    %Generate new individuals
-    tempPopulation = population;
-    for i = 1:2:populationSize
-        i1 = TournamentSelect(fitness, tournamentSelectionParameter, tournamentSize);
-        i2 = TournamentSelect(fitness, tournamentSelectionParameter, tournamentSize);
-        chromosome1 = population(i1,:);
-        chromosome2 = population(i2,:);
-
-        r = rand;
-        if (r < crossoverProbability)
-            newChromosomePair = Cross(chromosome1, chromosome2);
-            tempPopulation(i,:) = newChromosomePair(1,:);
-            tempPopulation(i+1,:) = newChromosomePair(2,:);
-        else
-            tempPopulation(i,:) = chromosome1;
-            tempPopulation(i+1,:) = chromosome2;
+        % Calculate fitness score for entire population
+        for i = 1:populationSize
+            chromosome = population(i,:);
+            x = DecodeChromosome(chromosome, variablesPerChromosome, variableRange);
+            decodedPopulation(i,:) = x;
+            fitness(i) = EvaluateIndividual(x);
+            % Save info about best individual
+                if (fitness(i) > maximumFitness)
+                    maximumFitness = fitness(i);
+                    bestIndividualIndex = i;
+                    xBest = x;
+                end
         end
-    end 
 
-    % Mutate new generation
-    for i = 1:populationSize
-        originalChromosome = tempPopulation(i,:);
-        mutatedChromosome = Mutate(originalChromosome, mutationProbability);
-        tempPopulation(i,:) = mutatedChromosome;
-    end
+        %Generate new individuals
+        tempPopulation = population;
+        for i = 1:2:populationSize
+            i1 = TournamentSelect(fitness, tournamentSelectionParameter, tournamentSize);
+            i2 = TournamentSelect(fitness, tournamentSelectionParameter, tournamentSize);
+            chromosome1 = population(i1,:);
+            chromosome2 = population(i2,:);
 
-    % Insert best individual to the nCopies first positions
-    tempPopulation = InsertBestIndividual(tempPopulation,...
-        population(bestIndividualIndex,:), nCopies);
-    population = tempPopulation;
+            r = rand;
+            if (r < crossoverProbability)
+                newChromosomePair = Cross(chromosome1, chromosome2);
+                tempPopulation(i,:) = newChromosomePair(1,:);
+                tempPopulation(i+1,:) = newChromosomePair(2,:);
+            else
+                tempPopulation(i,:) = chromosome1;
+                tempPopulation(i+1,:) = chromosome2;
+            end
+        end 
 
-    % Plot new generation
+        % Mutate new generation
+        for i = 1:populationSize
+            originalChromosome = tempPopulation(i,:);
+            mutatedChromosome = Mutate(originalChromosome, mutationProbability);
+            tempPopulation(i,:) = mutatedChromosome;
+        end
+
+        % Insert best individual to the nCopies first positions
+        tempPopulation = InsertBestIndividual(tempPopulation,...
+            population(bestIndividualIndex,:), nCopies);
+        population = tempPopulation;
+
+        % Plot new generation
 %         plotvector = get(bestPlotHandle, 'YData');
 %         plotvector(iGeneration) = maximumFitness;
 %         set(bestPlotHandle, 'YData', plotvector);
 %         set(textHandle, 'String', sprintf('best*: %4.3f', maximumFitness));
 %         set(populationPlotHandle, 'XData', decodedPopulation(:,1), 'YData', decodedPopulation(:,2), 'ZData', fitness(:));
 %         drawnow;
+    end
+    
+    if maximumFitness > globalMaximumFitness
+        globalMaximumFitness = maximumFitness;
+        globalXBest = xBest;
+    end
+    bestFitness(j) = maximumFitness;
+    minValue = 1/maximumFitness;
+    disp('minimum value');
+    disp(minValue);
+    disp('xBest');
+    disp(xBest);
+
 end
 
-
-
+minValue = 1/globalMaximumFitness;
+disp('minimum value');
+disp(minValue);
+disp('xBest');
+disp(globalXBest);
