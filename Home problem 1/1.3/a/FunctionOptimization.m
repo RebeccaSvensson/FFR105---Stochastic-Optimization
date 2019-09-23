d13f2e021eb185fd0ccb9d32567f641407922519
Rebecca Svensson <rebsven@student.chalmers.se>
2019-09-23 15:14:58 +0200

    1.3a updated to correspond with report

--- Home problem 1/1.3/a/FunctionOptimization.m
+++ Home problem 1/1.3/a/FunctionOptimization.m
@@ -12,48 +12,13 @@ numberOfGenerations = 500;
 tournamentSize = 4;
 nCopies = 2;
 nRuns = 10;
-bestFitness = zeros(nRuns, 1);
 
 globalMaximumFitness = 0;
 globalXBest = zeros(1,2); 
 
 for j = 1:nRuns
     fitness = zeros(populationSize,1);
-
     decodedPopulation = zeros(populationSize, 2);
-    % Init plots
-%     fitnessFigureHandle = figure;
-%     hold on;
-%     set(fitnessFigureHandle, 'Position', [50,50,500,200]);
-%     set(fitnessFigureHandle, 'DoubleBuffer', 'on');
-%     axis([1 numberOfGenerations 2.5 3]);
-%     bestPlotHandle = plot(1:numberOfGenerations, zeros(1, numberOfGenerations));
-%     textHandle = text(30, 2.6, sprintf('best: %4.3f', 0.0));
-%     hold off;
-%     drawnow;
-% 
-%     surfaceFigureHandle = figure;
-%     hold on;
-%     set(surfaceFigureHandle, 'DoubleBuffer', 'on');
-%     delta = 0.1;
-%     limit = fix(2*variableRange/delta) + 1;
-%     [xValues, yValues] = meshgrid(-variableRange:delta:variableRange, -variableRange:delta:variableRange);
-%     zValues = zeros(limit, limit);
-% 
-%     for j = 1:limit
-%         for k = 1:limit
-%             zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
-%         end
-%     end
-% 
-%     surfl(xValues, yValues, zValues)
-%     colormap gray;
-%     shading interp;
-%     view([-7 -9 10]);
-%     populationPlotHandle = plot3(decodedPopulation(:,1), decodedPopulation(:,2), fitness(:), 'kp');
-%     hold off; 
-%     drawnow;
-
 
     % Initialize population
     population = InitializePopulation(populationSize, numberOfGenes);
@@ -108,26 +73,12 @@ for j = 1:nRuns
         tempPopulation = InsertBestIndividual(tempPopulation,...
             population(bestIndividualIndex,:), nCopies);
         population = tempPopulation;
-
-        % Plot new generation
-%         plotvector = get(bestPlotHandle, 'YData');
-%         plotvector(iGeneration) = maximumFitness;
-%         set(bestPlotHandle, 'YData', plotvector);
-%         set(textHandle, 'String', sprintf('best*: %4.3f', maximumFitness));
-%         set(populationPlotHandle, 'XData', decodedPopulation(:,1), 'YData', decodedPopulation(:,2), 'ZData', fitness(:));
-%         drawnow;
     end
     
     if maximumFitness > globalMaximumFitness
         globalMaximumFitness = maximumFitness;
         globalXBest = xBest;
     end
-    bestFitness(j) = maximumFitness;
-    minValue = 1/maximumFitness;
-    disp('minimum value');
-    disp(minValue);
-    disp('xBest');
-    disp(xBest);
 
 end
 
